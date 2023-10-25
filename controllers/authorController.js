eea0d28aee70d811deae4ed9743599d855882192
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-10-25 14:42:47 +0200

    Add handler for author detail page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -4,4 +4,9 @@ const asyncHandler = require("express-async-handler");
 // Displaying list of all Authors
 exports.author_list = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author list");
-})
+});
+
+// Displaying detail page for specific Author
+exports.author_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Author Detail: ${req.params.id}`);
+});
