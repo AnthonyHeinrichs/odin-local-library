41f74df27694e7bc0caca8a75f515e1ab94761a8
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-11-07 16:30:14 +0100

    Add controller function for validating and saving new author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,6 @@
 const Author = require("../models/author");
 const Book = require("../models/book");
+const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 // Displaying list of all Authors
@@ -32,14 +33,56 @@ exports.author_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Displaying Author create form on Get 
-exports.author_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
-});
+exports.author_create_get = (req, res, next) => {
+  res.render("author_form", { title: "Create Author" });
+};
 
 // Handling Author creation on POST
-exports.author_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-});
+exports.author_create_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified"),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Create Author",
+        author: author,
+        errors: errors.array(),
+      });
+      return
+    } else {
+      await author.save()
+      res.redirect(author.url);
+    }
+  }),
+];
 
 // Displaying Author deletion form on GET
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
