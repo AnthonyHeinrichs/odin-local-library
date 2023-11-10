ccfa64b5cf98c5fc49efa9afbf0e827f38e6a766
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-11-10 14:29:05 +0100

    Add new virtual for formatting date to show on author update form

--- models/author.js
+++ models/author.js
@@ -31,9 +31,17 @@ AuthorSchema.virtual("date_of_birth_formatted").get(function () {
   DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
 });
 
+AuthorSchema.virtual("date_of_birth_form").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate();
+});
+
 AuthorSchema.virtual("date_of_death_formatted").get(function () {
   return this.date_of_death ?
   DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual("date_of_death_form").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate();
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
